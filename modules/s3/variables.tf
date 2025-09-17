e343138d2fffe8d339271f9f742c5defa9f258d6
Lucas Barros <lucas.barros@crmbonus.com>
2025-09-17 14:36:43 -0300

    confif cloudfront modules

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,9 +1,33 @@
-variable "bucket_name" { type = string }
-variable "tags" { type = map(string) default = {} }
+variable "bucket_name" {
+  type = string
+}
 
-variable "enable_website" { type = bool default = false }
-variable "index_document" { type = string default = "index.html" }
-variable "error_document" { type = string default = "error.html" }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
 
-variable "attach_bucket_policy" { type = bool default = true }
-variable "oai_iam_arn" { type = string default = null }
+variable "enable_website" {
+  type    = bool
+  default = false
+}
+
+variable "index_document" {
+  type    = string
+  default = "index.html"
+}
+
+variable "error_document" {
+  type    = string
+  default = "error.html"
+}
+
+variable "attach_bucket_policy" {
+  type    = bool
+  default = true
+}
+
+variable "oai_iam_arn" {
+  type    = string
+  default = null
+}
