63c41c2a29d7cd9a969580fbee05507e937a9665
Lucas Barros <lucas.barros@crmbonus.com>
2025-09-17 14:11:04 -0300

    confif cloudfront modules

--- modules/cloudfront/cloudfront.tf
+++ modules/cloudfront/cloudfront.tf
@@ -1,7 +1,3 @@
-resource "aws_cloudfront_origin_access_identity" "this" {
-  comment = "Projeto para ${var.project}"
-}
-
 resource "aws_cloudfront_distribution" "this" {
   enabled         = true
   is_ipv6_enabled = true
@@ -19,65 +15,56 @@ resource "aws_cloudfront_distribution" "this" {
     }
   }
 
-  default_root_object = var.default_root_object
-
   default_cache_behavior {
-    allowed_methods  = var.default_allowed_methods
-    cached_methods   = var.default_cached_methods
-    target_origin_id = var.origin_id
+    target_origin_id       = var.origin_id
+    viewer_protocol_policy = var.viewer_protocol_policy
 
-    viewer_protocol_policy = var.default_viewer_protocol_policy
+    allowed_methods  = var.allowed_methods
+    cached_methods   = var.cached_methods
 
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
+    dynamic "cache_policy_id" {
+      for_each = var.cache_policy_id != null ? [1] : []
+      content  = var.cache_policy_id
     }
 
-    min_ttl     = var.default_min_ttl
-    default_ttl = var.default_default_ttl
-    max_ttl     = var.default_max_ttl
+    dynamic "origin_request_policy_id" {
+      for_each = var.origin_request_policy_id != null ? [1] : []
+      content  = var.origin_request_policy_id
+    }
+
+    dynamic "response_headers_policy_id" {
+      for_each = var.response_headers_policy_id != null ? [1] : []
+      content  = var.response_headers_policy_id
+    }
   }
 
-  dynamic "ordered_cache_behavior" {
-    for_each = var.ordered_cache_behaviors
+  dynamic "restrictions" {
+    for_each = var.restriction_type != "none" ? [1] : []
     content {
-      path_pattern     = ordered_cache_behavior.value.path_pattern
-      allowed_methods  = ordered_cache_behavior.value.allowed_methods
-      cached_methods   = ordered_cache_behavior.value.cached_methods
-      target_origin_id = var.origin_id
-
-      viewer_protocol_policy = ordered_cache_behavior.value.viewer_protocol_policy
-
-      forwarded_values {
-        query_string = lookup(ordered_cache_behavior.value, "query_string", false)
-        cookies {
-          forward = "none"
-        }
-        headers = lookup(ordered_cache_behavior.value, "headers", [])
+      geo_restriction {
+        restriction_type = var.restriction_type
+        locations        = var.geo_locations
       }
-
-      min_ttl     = lookup(ordered_cache_behavior.value, "min_ttl", 0)
-      default_ttl = lookup(ordered_cache_behavior.value, "default_ttl", 3600)
-      max_ttl     = lookup(ordered_cache_behavior.value, "max_ttl", 86400)
-      compress    = lookup(ordered_cache_behavior.value, "compress", true)
     }
   }
 
-  viewer_certificate {
-    acm_certificate_arn = var.certificate_arn
-    ssl_support_method  = var.certificate_arn != null ? "sni-only" : null
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = var.restriction_type
-      locations        = var.geo_locations
+  dynamic "viewer_certificate" {
+    for_each = var.certificate_arn != null ? [1] : []
+    content {
+      acm_certificate_arn = var.certificate_arn
+      ssl_support_method  = "sni-only"
     }
   }
 
-  web_acl_id = var.web_acl_id
+  # WAF opcional
+  dynamic "web_acl_id" {
+    for_each = var.web_acl_id != null ? [1] : []
+    content  = var.web_acl_id
+  }
 
   tags = var.tags
+}
+
+resource "aws_cloudfront_origin_access_identity" "this" {
+  comment = "OAI for ${var.project}"
 }
