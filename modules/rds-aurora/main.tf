436335c56a86d8b81a0f13c00aa4848c1950d911
cassiapena <cassia.silva@crmbonus.com>
2025-12-10 17:50:54 -0300

    criação rds-aurora

--- modules/rds-aurora/main.tf
+++ modules/rds-aurora/main.tf
@@ -9,14 +9,15 @@ resource "aws_rds_cluster" "this" {
   preferred_maintenance_window = var.preferred_maintenance_window
   db_subnet_group_name    = aws_db_subnet_group.this.name
   vpc_security_group_ids  = [aws_security_group.this.id]
-  skip_final_snapshot       = var.skip_final_snapshot
-  final_snapshot_identifier = var.final_snapshot_identifier
-  
+
   scaling_configuration {
     min_capacity = var.min_capacity
     max_capacity = var.max_capacity
     auto_pause   = false
   }
+
+  skip_final_snapshot       = var.skip_final_snapshot
+  final_snapshot_identifier = var.final_snapshot_identifier
 
   tags = var.tags
 }
