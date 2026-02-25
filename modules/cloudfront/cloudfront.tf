d4cb9f31cc2dfc4043845b925e4345e6b59c7d08
cassiapena <cassia.silva@crmbonus.com>
2026-02-25 13:43:38 -0300

    Update cloudfront.tf

--- modules/cloudfront/cloudfront.tf
+++ modules/cloudfront/cloudfront.tf
@@ -1,6 +1,7 @@
 resource "aws_cloudfront_origin_access_identity" "this" {
   comment = "OAI for ${var.project}"
 }
+
 resource "aws_cloudfront_distribution" "this" {
   enabled         = true
   is_ipv6_enabled = true
@@ -31,18 +32,7 @@ resource "aws_cloudfront_distribution" "this" {
     response_headers_policy_id = var.response_headers_policy_id != null ? var.response_headers_policy_id : null
   }
 
-  # Usando ordered_cache_behaviors (em vez de dynamic ou cache_behaviors)
-  ordered_cache_behaviors {
-    for_each = var.custom_cache_behaviors
-    path_pattern               = each.value.path_pattern
-    target_origin_id           = each.value.target_origin_id
-    viewer_protocol_policy     = each.value.viewer_protocol_policy
-    allowed_methods            = each.value.allowed_methods
-    cached_methods             = each.value.cached_methods
-    cache_policy_id            = each.value.cache_policy_id
-    origin_request_policy_id   = each.value.origin_request_policy_id
-    response_headers_policy_id = each.value.response_headers_policy_id
-  }
+
 
   restrictions {
     geo_restriction {
